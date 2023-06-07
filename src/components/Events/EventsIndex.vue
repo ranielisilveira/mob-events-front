cade2a794d18a0533feb6e5f2f6fd064adf84733
Ranieli <ranielisilveira@hotmail.com>
2023-06-06 21:43:55 -0300

    feat: Creating view and index

---
+++ src/components/Events/EventsIndex.vue
@@ -0,0 +1,11 @@
+<template>
+  <events-list />
+</template>
+
+<script>
+import EventsList from "./EventsList.vue";
+export default {
+  components: { EventsList },
+  name: "EventsIndex",
+};
+</script>
---
+++ src/views/EventsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <events-index />
+</template>
+
+<script>
+import EventsIndex from "@/components/Events/EventsIndex.vue";
+
+export default {
+  name: "EventsView",
+
+  components: {
+    EventsIndex,
+  },
+};
+</script>
