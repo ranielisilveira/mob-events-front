5df67127cfa30dec14bdfc0388ddfd8d0b3f3cd0
Ranieli <ranielisilveira@hotmail.com>
2023-06-06 21:44:31 -0300

    feat: :construction: Initial structure todo list events

---
+++ src/components/Events/EventsList.vue
@@ -0,0 +1,239 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-row class="pl-4">
+        <v-col cols="12">
+          <h1>Eventos</h1>
+        </v-col>
+      </v-row>
+      <v-card class="ml-3 mt-3 mr-3 mb-3">
+        <v-row no-gutters>
+          <v-col cols="12" class="fixture-names pl-4 pt-2"> Filtros </v-col>
+        </v-row>
+        <v-card-text>
+          <v-row>
+            <v-col cols="3">
+              <v-text-field
+                label="Nome do convidado"
+                outlined
+                dense
+              ></v-text-field>
+            </v-col>
+            <v-col cols="3">
+              <v-menu v-model="menu1" close-on-content-click max-width="290">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    :value="null"
+                    label="Data inicial"
+                    readonly
+                    dense
+                    outlined
+                    v-bind="attrs"
+                    v-on="on"
+                    @click:clear="date = null"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="date"
+                  @change="menu1 = false"
+                ></v-date-picker>
+              </v-menu>
+            </v-col>
+            <v-col cols="3">
+              <v-menu v-model="menu1" close-on-content-click max-width="290">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    :value="null"
+                    label="Data Final"
+                    readonly
+                    dense
+                    outlined
+                    v-bind="attrs"
+                    v-on="on"
+                    @click:clear="date = null"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="date"
+                  @change="menu1 = false"
+                ></v-date-picker>
+              </v-menu>
+            </v-col>
+            <v-col cols="3">
+              <v-text-field
+                v-model="search"
+                label="Buscar"
+                append-icon="mdi-magnify"
+                outlined
+                dense
+                hide-details
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+      <v-row no-gutters>
+        <v-container>
+          <v-row no-gutters>
+            <v-col cols="5" class="fixture-names pl-4 pt-2"> Nome </v-col>
+            <v-col cols="2" class="fixture-names pl-4 pt-2">
+              Data inicio
+            </v-col>
+            <v-col cols="2" class="fixture-names pl-4 pt-2">
+              Data inicio
+            </v-col>
+            <v-col cols="1" class="fixture-names pl-4 pt-2"> Duração </v-col>
+            <v-col cols="1" class="fixture-names pl-4 pt-2"> Editar </v-col>
+            <v-col cols="1" class="fixture-names pl-4 pt-2"> Excluir </v-col>
+          </v-row>
+          <div v-for="event in events" :key="event.id">
+            <v-row no-gutters class="item-text">
+              <v-col cols="5" class="pl-4 pt-4">
+                {{ event.title }}
+              </v-col>
+              <v-col cols="2" class="pl-5 pt-4">
+                {{ formatedDate(event.start_event) }}
+              </v-col>
+              <v-col cols="2" class="pl-5 pt-4">
+                {{ formatedDate(event.end_event) }}
+              </v-col>
+              <v-col cols="1" class="pl-5 pt-4">
+                {{ calculateDuration(event.start_event, event.end_event) }} dias
+              </v-col>
+              <v-col cols="1" class="pl-2 pt-4">
+                <v-btn color="primary" text>
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+              </v-col>
+              <v-col cols="1" class="pl-3 pt-4">
+                <v-btn color="error" text>
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+            <v-divider color="grey" class="divider-point"></v-divider>
+          </div>
+        </v-container>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  name: "EventsList",
+
+  data() {
+    return {
+      events: [
+        {
+          id: 1,
+          title: "Evento 1",
+          start_event: "2021-08-01 00:00:00",
+          end_event: "2021-08-03 00:00:00",
+        },
+        {
+          id: 2,
+          title: "Evento 2",
+          start_event: "2021-07-01",
+          end_event: "2021-08-01",
+        },
+        {
+          id: 3,
+          title: "Evento 3",
+          start_event: "2021-08-01",
+          end_event: "2021-08-01",
+        },
+        {
+          id: 4,
+          title: "Evento 4",
+          start_event: "2021-08-01",
+          end_event: "2021-08-01",
+        },
+        {
+          id: 5,
+          title: "Evento 5",
+          start_event: "2021-08-01",
+          end_event: "2021-08-01",
+        },
+        {
+          id: 6,
+          title: "Evento 6",
+          start_event: "2021-08-01",
+          end_event: "2021-08-01",
+        },
+        {
+          id: 7,
+          title: "Evento 7",
+          start_event: "2021-08-01",
+          end_event: "2021-08-01",
+        },
+        {
+          id: 8,
+          title: "Evento 8",
+          start_event: "2021-08-01",
+          end_event: "2021-08-01",
+        },
+        {
+          id: 9,
+          title: "Evento 9",
+          start_event: "2021-08-01",
+          end_event: "2021-08-01",
+        },
+        {
+          id: 10,
+          title: "Evento 10",
+          start_event: "2021-08-01",
+          end_event: "2021-08-01",
+        },
+        {
+          id: 11,
+          title: "Evento 11",
+          start_event: "2021-08-01",
+          end_event: "2021-08-01",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    computedDateFormattedMomentjs(date) {
+      const dateFormated = moment(date).format("DD/MM/YYYY");
+      return dateFormated;
+    },
+
+    calculateDuration(start, end) {
+      const startEvent = moment(start);
+      const endEvent = moment(end);
+      const duration = moment.duration(endEvent.diff(startEvent));
+      const days = duration.asDays();
+      return days;
+    },
+
+    formatedDate(date) {
+      const dateFormated = moment(date).format("DD/MM/YYYY");
+      return dateFormated;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fixture-names {
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: #060452;
+}
+
+.divider-point {
+  margin: 10px;
+  margin-bottom: 15px;
+}
+
+.item-text {
+  font-size: 1.1rem;
+  font-weight: 500;
+  color: #69686e;
+}
+</style>
