4eec4c954d8905d85dc81237b887308bd6062ec9
Ranieli <ranielisilveira@hotmail.com>
2023-06-09 18:12:46 -0300

    feat: Create flow to register attendance

--- src/components/Modals/RegisterAttendanceModal.vue
+++ src/components/Modals/RegisterAttendanceModal.vue
@@ -15,44 +15,53 @@
         <v-form>
           <v-row no-gutters class="pt-5">
             <v-col md="6" sm="12" xs="12" offset-md="3">
-              Nome dele
-              <p>CPF dele</p>
+              {{ participant.name }}
+              <p>CPF: {{ participant.cpf }}</p>
             </v-col>
           </v-row>
           <v-row no-gutters class="pt-5">
-            <v-col md="6" sm="12" xs="12" offset-md="3">
-              <v-menu v-model="menu1" close-on-content-click max-width="290">
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    :value="null"
-                    label="Data"
-                    readonly
-                    dense
-                    outlined
-                    v-bind="attrs"
-                    v-on="on"
-                    @click:clear="date = null"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  v-model="date"
-                  @change="menu1 = false"
-                ></v-date-picker>
-              </v-menu>
-            </v-col>
+            <v-menu
+              v-model="date"
+              :close-on-content-click="false"
+              transition="scale-transition"
+              offset-y
+              min-width="auto"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="dateFormatted"
+                  label="Data do evento *"
+                  readonly
+                  dense
+                  outlined
+                  v-bind="attrs"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="dateEvent"
+                :return-value.sync="minDate"
+                :min="minDate"
+              ></v-date-picker>
+            </v-menu>
           </v-row>
         </v-form>
       </v-card-text>
 
       <v-card-actions>
         <v-col md="6" sm="12" xs="12" offset-md="5" offset-sm="4">
+          <v-snackbar v-model="snackbar.show" :timeout="3000">
+            {{ snackbar.message }}
+          </v-snackbar>
+
           <v-btn
+            :disabled="!isFilled"
             class="white--text text-none"
-            @click="register"
-            :loading="saving"
-            :disabled="!formValid"
-            >Salvar</v-btn
+            color="green"
+            @click="saveAttendance"
           >
+            <span>Salvar</span>
+          </v-btn>
         </v-col>
       </v-card-actions>
     </v-card>
@@ -60,14 +69,27 @@
 </template>
 
 <script>
+import moment from "moment";
 export default {
   name: "RegisterAttendanceModal",
   data() {
     return {
       isOpen: false,
-      participant: {},
-      saving: false,
-      formValid: false,
+      date: false,
+      dateEvent: null,
+      snackbar: {
+        show: false,
+        color: "",
+        message: "default",
+      },
+      attendance: {
+        participant_id: "",
+        event_date: "",
+      },
+      parti: {},
+      minDate: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
     };
   },
 
@@ -75,6 +97,11 @@ export default {
     dialog: {
       type: Boolean,
       default: false,
+    },
+
+    participant: {
+      type: Object,
+      default: () => {},
     },
   },
 
@@ -90,9 +117,45 @@ export default {
     },
   },
 
+  computed: {
+    isFilled() {
+      return this.dateEvent !== "";
+    },
+
+    dateFormatted() {
+      return moment(this.dateEvent).isValid()
+        ? moment(this.dateEvent).format("DD/MM/YYYY")
+        : "";
+    },
+  },
+
   methods: {
     close() {
-      this.isOpen = false;
+      this.$emit("close");
+    },
+
+    messsageSuccess() {
+      this.snackbar.show = true;
+      this.snackbar.color = "success";
+      this.snackbar.message = "Evento cadastrado com sucesso!";
+    },
+
+    async saveAttendance() {
+      this.attendance.participant_id = this.participant.id;
+      this.attendance.event_date = this.dateEvent;
+
+      try {
+        await this.$axios.post("/attendance", this.attendance).then(() => {
+          this.attendance = {
+            participant_id: "",
+            event_id: "",
+          };
+          this.isOpen = false;
+        });
+        this.messsageSuccess();
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
