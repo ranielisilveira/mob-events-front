0083317b3f2c3e10ebe1f017dcbf24c25c4c107a
Ranieli <ranielisilveira@hotmail.com>
2023-06-09 08:01:56 -0300

    feat: Create component event details

---
+++ src/components/Registers/PageRegister.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-row class="pl-4">
+        <v-col cols="12">
+          <h1>Detalhes do Evento</h1>
+        </v-col>
+      </v-row>
+      <tabs-fixtures />
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import TabsFixtures from "../Tabs/TabsFixtures.vue";
+export default {
+  name: "PageRegister",
+  components: {
+    TabsFixtures,
+  },
+};
+</script>
+
+<style scoped>
+.fixture-names {
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: #060452;
+}
+
+.divider-point {
+  margin: 10px;
+  margin-bottom: 15px;
+}
+
+.item-text {
+  font-size: 1.1rem;
+  font-weight: 500;
+  color: #69686e;
+}
+</style>
